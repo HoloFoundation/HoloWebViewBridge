ede6961d449cd1457588a3c6d6c50118c31bc8d0
gonghonglou <gonghonglou@icloud.com>
2021-01-29 17:26:56 +0800

    update js

--- HoloWebViewBridge/Assets/main.js
+++ HoloWebViewBridge/Assets/main.js
@@ -1,39 +1,39 @@
 // main.js
 
 if (!window.bridge) {
-    window.bridge = {};
+    window.bridge = {}
 }
 
 window.bridge.js_funSend = {
     __count: 0,
     cache: {},
     invoke: function invoke(id, args) {
-        var key = "" + id;
-        var func = window.bridge.js_funSend.cache[key];
-        func(args);
+        var key = "" + id
+        var func = window.bridge.js_funSend.cache[key]
+        func(args)
     },
     push: function push(func) {
-        var index = -1;
+        var index = -1
         if (func != null) {
-            window.bridge.js_funSend.__count += 1;
-            index = window.bridge.js_funSend.__count;
-            window.bridge.js_funSend.cache["" + index] = func;
+            window.bridge.js_funSend.__count += 1
+            index = window.bridge.js_funSend.__count
+            window.bridge.js_funSend.cache["" + index] = func
         }
-        return index;
+        return index
     }
-};
+}
 
 window.bridge.js_msgSend = function(id, selector) {
     var length = arguments.length, args = Array(length > 2 ? length - 2 : 0)
     for (key = 2; key < length; key++) {
-        args[key - 2] = arguments[key];
+        args[key - 2] = arguments[key]
     }
 
     args = args.map(function(elt) {
         if (typeof elt === "function") {
-            return { flags: 1, val: window.bridge.js_funSend.push(elt) };
+            return { flags: 1, val: window.bridge.js_funSend.push(elt) }
         } else {
-            return { flags: 0, val: elt };
+            return { flags: 0, val: elt }
         }
     })
     
@@ -42,4 +42,4 @@ window.bridge.js_msgSend = function(id, selector) {
         selector: selector,
         args: args
     })
-};
+}
