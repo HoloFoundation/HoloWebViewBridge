269abd027687f771eae9bfda300988e44cc56d72
gonghonglou <gonghonglou@icloud.com>
2021-01-29 16:54:18 +0800

    Code comments

--- HoloWebViewBridge/Classes/Core/WebViewPluginProtocol.swift
+++ HoloWebViewBridge/Classes/Core/WebViewPluginProtocol.swift
@@ -14,10 +14,30 @@ public typealias ResponseCallbackHandler = ((_ args: [Any], _ callback: Response
 
 public protocol WebViewPluginProtocol {
     
+    /// Unique identity
+    /// Make sure that the identity of the plug-in you add is unique
+    ///
+    /// Example:
+    /// holo.webView.bridge.log
     var identifier: String { get }
     
+    /// JavaScript code
+    /// The JavaScript code is going to be injected into the WebView
+    ///
+    /// Example:
+    /// window.bridge.log = function(msg) {
+    ///     window.bridge.js_msgSend("holo.webView.bridge.log", "log()", msg)
+    /// }
     var javascript: String { get }
     
+    /// This method is called when JavaScript executes the injected method
+    /// - Parameters:
+    ///   - fun: Identifies the method executed by JavaScript
+    ///   - args: Parameter passed to the method executed by JavaScript
+    ///
+    /// Example:
+    /// JS:         window.bridge.log("hello")
+    /// Native:     didReceiveMessage("log()", args: ["hello"])
     func didReceiveMessage(_ fun: String, args: [Any])
     
 }
