3e6e3cd5e0b64d2ac88dbd0294d3320fe5ba0960
gonghonglou <gonghonglou@icloud.com>
2021-02-04 23:58:22 +0800

    code comment

--- HoloWebViewBridge/Classes/Core/WebViewPluginProtocol.swift
+++ HoloWebViewBridge/Classes/Core/WebViewPluginProtocol.swift
@@ -34,17 +34,17 @@ public protocol WebViewPluginProtocol {
     /// Unique identity
     /// Make sure that the identity of the plug-in you add is unique
     ///
-    /// Example:
-    /// holo.webView.bridge.log
+    ///     Example:
+    ///     holo.webView.bridge.log
     var identifier: String { get }
     
     /// JavaScript code
     /// The JavaScript code is going to be injected into the WebView
     ///
-    /// Example:
-    /// window.bridge.log = function(msg) {
-    ///     window.bridge.js_msgSend("holo.webView.bridge.log", "log()", msg)
-    /// }
+    ///     Example:
+    ///     window.bridge.log = function(msg) {
+    ///         window.bridge.js_msgSend("holo.webView.bridge.log", "log()", msg)
+    ///     }
     var javascript: String { get }
     
     /// This method is called when JavaScript executes the injected method
@@ -52,13 +52,13 @@ public protocol WebViewPluginProtocol {
     ///   - fun: Identifies the method executed by JavaScript
     ///   - args: Parameter passed to the method executed by JavaScript
     ///
-    /// Example 1:
-    /// JS:         window.bridge.log("hello")
-    /// Native:     didReceiveMessage("log()", args: "hello")
+    ///         Example 1:
+    ///         JS:     window.bridge.log("hello")
+    ///         Native: didReceiveMessage("log()", args: "hello")
     ///
-    /// Example 2:
-    /// JS:         window.bridge.log("hello", "word")
-    /// Native:     didReceiveMessage("log()", args: ["hello", "word"])
+    ///         Example 2:
+    ///         JS:     window.bridge.log("hello", "word")
+    ///         Native: didReceiveMessage("log()", args: ["hello", "word"])
     func didReceiveMessage(_ fun: String, args: Any?)
     
 }
